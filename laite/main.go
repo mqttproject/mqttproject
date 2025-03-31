3f8506f29408c47e7e0bcb3b26a0e8eaa5970996
jan5u <janhenrik02@hotmail.fi>
2025-03-31 22:47:12 +0300

    Add device shutdown logic

--- laite/main.go
+++ laite/main.go
@@ -5,36 +5,42 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-);
-
+)
 
 func main() {
-	generalConf,devicesConf , err := loadConf("devices.toml")
-	if err != nil{
-		fmt.Println("err in reading devices ",err);
+	generalConf, devicesConf, err := loadConf("devices.toml")
+	if err != nil {
+		fmt.Println("err in reading devices ", err)
 	}
-	deviceInterface := generalConf.INTERFACE;
-	ipStart := generalConf.IPSTART;
-	ipEnd := generalConf.IPEND;
+	deviceInterface := generalConf.INTERFACE
+	ipStart := generalConf.IPSTART
+	ipEnd := generalConf.IPEND
 
 	defer cleanNetworking(deviceInterface)
+
+	var runningDevices []*Device
 
 	for _, config := range devicesConf {
 		action, found := actionMap[config.Action]
 		if !found {
 			continue
 		}
-		device,err := createDevice(config.ID,config.Broker, action,deviceInterface,ipStart,ipEnd)
-		if err != nil{
+		device, err := createDevice(config.ID, config.Broker, action, deviceInterface, ipStart, ipEnd)
+		if err != nil {
 			continue
 		}
 		deviceOn(&device)
+		runningDevices = append(runningDevices, &device)
 	}
 
 	go startAPI()
 
-
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-	<-sigChan  
+	<-sigChan
+
+	fmt.Println("Shutting down devices...")
+	for _, device := range runningDevices {
+		deviceOff(device)
+	}
 }
